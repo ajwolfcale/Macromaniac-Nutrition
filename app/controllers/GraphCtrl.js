bb5382006c53c87fd04c8c2fed84760158ad1caa
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-02-08 11:05:10 -0600

    updating charts

--- app/controllers/GraphCtrl.js
+++ app/controllers/GraphCtrl.js
@@ -1,8 +1,71 @@
 'use strict';
 
-angular.module("NutritionApp").controller("GraphCtrl", function($scope){
-    $scope.labels = ["Protein", "Fat", "Carbs"];
-    $scope.data = [300, 500, 100];
+
+angular.module("NutritionApp").controller("GraphCtrl", function ($scope, NutritionFactory, ProfileFactory) {
+
+	let calArr =[];
+	let proteinArr =[];
+	let fatArr =[];
+	let carbArr =[];
+	let getTotal = (total, num) => {
+		return total + num;
+	};
+
+	$scope.graphNutrients = () => {
+		// console.log("getting nutrients");
+		NutritionFactory.getNutrients()
+			.then(function (results) {
+				console.log("CHECK THIS", results);
+				let nutrientsByDate = results.map((item) => {
+					// console.log("Item", item);
+					let todaysDate = item.date;
+					calArr.push(item.calories);
+					proteinArr.push(item.protein);
+					fatArr.push(item.fat);
+					carbArr.push(item.carbs);
+					console.log("TODAY'S DATE", todaysDate);
+					// console.log("TOTAL CALORIES: ", calArr.reduce(getTotal));
+					// console.log("TOTAL PROTEIN: ", proteinArr.reduce(getTotal));
+					// console.log("TOTAL FAT: ", fatArr.reduce(getTotal));
+					// console.log("TOTAL CARBS: ", carbArr.reduce(getTotal));
+					$scope.totalCalories = calArr.reduce(getTotal);
+					$scope.totalProtein = proteinArr.reduce(getTotal);
+					$scope.totalFat = fatArr.reduce(getTotal);
+					$scope.totalCarbs = carbArr.reduce(getTotal);
+					$scope.pieData = [$scope.totalProtein, $scope.totalFat, $scope.totalCarbs];
+				});
+			})
+			.catch(err => console.error(err));
+	};
+
+	// ---------------------------------PIE CHART---------------------------------
+
+	$scope.pieLabels = ["Protein", "Fat", "Carbs"];
+	// $scope.pieData = [$scope.totalProtein, $scope.totalFat, $scope.totalCarbs];
+	$scope.pieData = [1, 1, 1];
+	
+
+	// ---------------------------------LINE CHART---------------------------------
+	$scope.lineLabels = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
+	$scope.lineSeries = ['Calorie Goal', 'Calories Eaten'];
+	$scope.lineData = [
+		[2000, 2000, 2000, 2000, 2000, 2000, 2000],
+		[2500, 2300, 3000, 2456, 1986, 2000, 3090]
+	];
+
+	$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+	$scope.options = {
+		scales: {
+			yAxes: [
+				{
+					id: 'y-axis-1',
+					type: 'linear',
+					display: true,
+					position: 'left'
+				}
+			]
+		}
+	};
 });
 
 
--- app/factories/NutritionFactory.js
+++ app/factories/NutritionFactory.js
@@ -1,12 +1,12 @@
 'use strict';
 
 
-angular.module("NutritionApp").factory("NutritionFactory", function ($q, $http, FoodCreds) {
+angular.module("NutritionApp").factory("NutritionFactory", function ($q, $http, FoodCreds, AuthFactory, FBUrl) {
 
+// --------------------------------Nutrition API Call--------------------------------
 	let getFoods = (foods) => {
 		return $q(function (resolve, reject) {
 			$http.get(`https://api.nutritionix.com/v1_1/search/${foods}?results=0%3A50&cal_min=0&cal_max=50000&fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein&appId=${FoodCreds.app_id}&appKey=${FoodCreds.key}`)
-
 				.then(function (data) {
 					resolve(data);
 				})
@@ -15,9 +15,29 @@ angular.module("NutritionApp").factory("NutritionFactory", function ($q, $http, 
 				});
 		});
 	};
-	return {
-		getFoods
+
+// -----------------------------Firebase Data Call--------------------
+	let getNutrients = () => {
+		return $q(function (resolve, reject) {
+			let currentUser = AuthFactory.getCurrentUser();
+			console.log("Current User:", currentUser);
+			$http.get(`${FBUrl}nutrientsToday.json?&orderBy="uid"&equalTo="${currentUser}"`)
+				.then(({ data }) => {
+					// console.log("data", data);
+					let nutrientArr = Object.values(data).map(nutrients => {
+						data[nutrients] = nutrients;
+						return data[nutrients];
+					});
+					// console.log("nutrients", nutrientArr);
+					resolve(nutrientArr);
+				})
+				.catch(function (error) {
+					reject(error);
+				});
+		});
 	};
+
+	return { getFoods, getNutrients };
 
 });
 
